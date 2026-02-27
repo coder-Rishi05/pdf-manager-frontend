ee5fa643cb2aaa06cbb24a1ffced87db124ae033
coder-Rishi05 <rawatrishi1121@gmail.com>
2026-02-27 23:34:06 +0530

    added about page

---
+++ src/components/layouts/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <header>Why To Choose us ?</header>
+      <main>01. We design Unique Web/Graphic Experince</main>
+    </div>
+  );
+};
+
+export default About;
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div>
-      <h1>Welcome Home</h1>
+      {/* <h1>Welcome Home</h1>
       <Link to={"/create-post"} >Create post</Link>
-      <Link to={"/feed"} >Get Post</Link>
-      <div className="w-full h-screen bg-zinc-800">
-        <h1 className='font-[Open] text-8xl ' >PDF MANAGER</h1>
+      <Link to={"/feed"} >Get Post</Link> */}
+      <div className="w-full h-[200vh] bg-neutral-700/20">
+        <div className="w-full  flex items-center flex-col justify-center">
+          <div className="center flex items-center justify-center">
+            <h1 className="text-zinc-900/90 font-[Open] font-bold text-[30vh] leading-44 tracking-tight ">
+              PDF MANAGER
+            </h1>
+          </div>
+          <div className="center flex items-center justify-center">
+            <h1 className="text-zinc-900/90 font-[Open] font-bold text-[30vh] leading-44 tracking-tight ">
+              An Web Made for 
+              You cloud Storage
+            </h1>
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
