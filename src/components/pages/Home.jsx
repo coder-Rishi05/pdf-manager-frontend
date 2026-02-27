92b383d201b436bbb46c363022fa868e21667887
coder-Rishi05 <rawatrishi1121@gmail.com>
2026-02-27 23:17:35 +0530

    setup tailwind

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -7,6 +7,9 @@ const Home = () => {
       <h1>Welcome Home</h1>
       <Link to={"/create-post"} >Create post</Link>
       <Link to={"/feed"} >Get Post</Link>
+      <div className="w-full h-screen bg-zinc-800">
+        <h1 className='font-[Open] text-8xl ' >PDF MANAGER</h1>
+      </div>
     </div>
   )
 }
